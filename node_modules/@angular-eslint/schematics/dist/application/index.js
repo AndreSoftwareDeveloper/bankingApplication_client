72749f643dac9f49f598ec66cb59419522af5d0e
YoungNavigator <andriej2301@gmail.com>
2024-06-18 11:56:33 +0200

    chore: Updated turbo.json

---
+++ node_modules/@angular-eslint/schematics/dist/application/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const devkit_imports_1 = require("../devkit-imports");
+const utils_1 = require("../utils");
+exports.default = (0, devkit_imports_1.convertNxGenerator)(async (tree, options) => {
+    // Remove angular-eslint specific options before passing to the Angular schematic
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { setParserOptionsProject, ...angularOptions } = options;
+    const applicationGenerator = (0, devkit_imports_1.wrapAngularDevkitSchematic)('@schematics/angular', 'application');
+    await applicationGenerator(tree, angularOptions);
+    // Update the lint builder and config in angular.json
+    (0, utils_1.addESLintTargetToProject__NX)(tree, options.name, 'lint');
+    (0, utils_1.createESLintConfigForProject__NX)(tree, options.name, options.setParserOptionsProject ?? false);
+});
